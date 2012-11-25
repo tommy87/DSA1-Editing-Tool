96ae5326ed5d725ff255fdd7c6a82ef8cfe059f6
tommy87 <my_spam1@gmx.net>
2012-11-25 10:43:37 -0800

    -neue Klasse für Standardfarben wurden hinzugefügt

---
+++ DSA 1 Editing Tool/Colors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Drawing;
+
+namespace DSA_1_Editing_Tool
+{
+    public static class Colors
+    {
+        public static Color keyColor_Fight = Color.LightSeaGreen;
+        public static Color keyColor_Door = Color.RoyalBlue;
+        public static Color keyColor_Stair = Color.LightSalmon;
+    }
+}
